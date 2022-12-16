97176cbffa7ce67139cf37595729bb2405af4f41
anoopraju31 <59496739+anoopraju31@users.noreply.github.com>
2022-12-16 19:08:29 +0530

    Update settings.py

--- movies/settings.py
+++ movies/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cr3)2rnm5v3al5hf^ok8q69)x9_#d$_ty(t8@si@#-02=01d+^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
